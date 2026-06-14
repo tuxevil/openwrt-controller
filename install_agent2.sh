7155b8cea48c2d298b15b0d3c29b89986966537f
Sebastian Real <tuxevil@gmail.com>
2026-06-14 11:19:40 -0500

    chore: remove temporary debug scripts

--- install_agent2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cat devices/agent.sh | ssh -o StrictHostKeyChecking=no root@10.0.0.3 "cat > /tmp/agent.sh"
-ssh -o StrictHostKeyChecking=no root@10.0.0.3 "sed -i 's/TU_API_KEY_AQUI/a29c9e4cb27c6a350b7d0d9dbf70ac03/g' /tmp/agent.sh"
-ssh -o StrictHostKeyChecking=no root@10.0.0.3 "sed -i 's/10.0.0.6/10.0.0.6/g' /tmp/agent.sh"
-ssh -o StrictHostKeyChecking=no root@10.0.0.3 "chmod +x /tmp/agent.sh && nohup /tmp/agent.sh > /dev/null 2>&1 &"
